7e933ba5661078021d38a2a8ad25861cc2e481e5
ariffnazhan-scicom <ariff.asri@scicom.com.my>
2026-03-13 16:32:13 +0800

    clean code

--- stt_api/livekit_plugin/__init__.py
+++
@@ -1,3 +0,0 @@
-from .vad_plugin import VAD, VADStream
-
-__all__ = ["VAD", "VADStream"]
--- stt_api/livekit_plugin/vad_plugin/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Custom Silero VAD plugin for LiveKit Agents with concurrency-limited inference."""
-
-from .vad import VAD, VADStream
-
-__all__ = ["VAD", "VADStream"]
